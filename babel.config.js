904b6ea81e2896f0ebf597ebf6287e3f92733e3a
Adarsh Gupta <blueadarsh1@gmail.com>
2025-09-26 20:19:36 +0000

    face and id verification

--- babel.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    // Add any babel plugins needed for the project
-    'react-native-reanimated/plugin', // Should be last
-  ],
-};
